15323a6c401b8a96ed66299666cb2087f9a62560
Robert Mitchell <robert.mitchell36@gmail.com>
2016-10-25 19:50:10 -0500

    Added Java Code
    Added the work-in-progress implementation of the racket code in Java.

---
+++ Patterns/src/patterns/Scalar.java
@@ -0,0 +1,52 @@
+package patterns;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author robertmitchell
+ * @param <T> The type of the scalar
+ */
+public class Scalar<T> implements Expression {
+    private final T value;
+    
+    public Scalar(final T value) {
+        this.value = value;
+    }
+
+    @Override
+    public Operator getOperator() {
+        return null;
+    }
+
+    @Override
+    public List<Expression> getSubExpressions() {
+        return new ArrayList<>();
+    }
+    
+    public T getValue() {
+        return this.value;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        if (other == null || !(other instanceof Scalar)) {
+            return false;
+        }
+        return this.value.equals(((Scalar) other).value);
+    }
+ 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.value);
+        return hash;
+    }
+   
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
