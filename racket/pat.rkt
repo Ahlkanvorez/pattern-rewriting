602015c11d64ec49607dafcc8a67cff7c142bc29
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-08 18:09:14 -0500

    Made pat.rkt conform to an 80 character max line length.

--- racket/pat.rkt
+++ racket/pat.rkt
@@ -42,7 +42,10 @@
 ;;  ;; > (substitute '((Hello Farewell) (friend sir)) '(Hello friend)
 ;;  ;; '(Farewell sir)
     (define (substitute-test)
-      (equal? (substitute '((Nihil nothing) (sub under) (sole (the sun)) (novum (is new)))
+      (equal? (substitute '((Nihil nothing)
+                            (sub under)
+                            (sole (the sun))
+                            (novum (is new)))
                           '(Nihil sub sole novum)) ; Ecclesiastes
               '(nothing under (the sun) (is new))))
 (define (substitute dict a)
@@ -59,17 +62,21 @@
       a))
 
 ; Patterns of the form '((x? y?) (z? w?) (a? b?))
-;;;; '((a? b?) (x? y?) (z? w?)) '(a list of lists and symbols) -> '(a new list transformed according to the given patterns)
-;; Given a list of patterns, which allows for matching structural form instead of simply
-;; testing equality of symbols, and a list of symbols and lists, the original list is transformed
-;; according to the structural patterns provided, and the new list is returned. This transforms
-;; the entire expression tree  by considering every branch and node, not simply the root.
+;;;; '((a? b?) (x? y?) (z? w?)) '(a list of lists and symbols)
+;;;;    -> '(a new list transformed according to the given patterns)
+;; Given a list of patterns, which allows for matching structural form instead
+;; of simply testing equality of symbols, and a list of symbols and lists, the
+;; original list is transformed according to the structural patterns provided,
+;; and the new list is returned. This transforms the entire expression tree
+;; by considering every branch and node, not simply the root.
 ;;; Example-usage: (transform-test)
-;;  ;; > (transform '(((if condition? result?) (result? unless (not condition?))))
+;;  ;; > (transform '(((if condition? result?)
+;;  ;;                 (result? unless (not condition?))))
 ;;  ;;              '(if (you are hungry) (eat some food)))
 ;;  ;; '((eat some food) unless (not (you are hungry)))
     (define (transform-test)
-      (transform '(((if condition? result?) (result? unless (not condition?))))
+      (transform '(((if condition? result?)
+                    (result? unless (not condition?))))
                  '(if (you are hungry) (eat some food))))
 (define (transform patterns a)
   (if (null? patterns)
@@ -77,17 +84,19 @@
       (transform (cdr patterns) (transform-part (car patterns) a))))
 
 ;;;; (patterns expression) -> expression
-;; This function continues to transform the list using the transform function until the list
-;; does not change, or until the resulting transformation has been seen previously.
+;; This function continues to transform the list using the transform function
+;; until the list does not change, or until the resulting transformation has
+;; been seen previously.
 ;;; Example-usage: (transform-exhaustive-test)
 ;;  ;; > (transform-exhaustive '(((S (N (S x?))) (N x?)) ((N (N x?)) (I x?))
 ;;  ;;                           ((S (I x?)) (S x?)) ((N (I x?) (N x?))))
 ;;  ;;                         '(S (S (S (N (S (N (N (S (S (N x?)))))))))))
 ;;  ;; 0
 (define (transform-exhaustive-test)
-  (let ((result (transform-exhaustive '(((S (N (S x?))) (N x?)) ((N (N x?)) (I x?))
-                                        ((S (I x?)) (S x?)) ((N (I x?) (N x?))))
-                                      '(S (S (S (N (S (N (N (S (S (N x?))))))))))))
+  (let ((result (transform-exhaustive
+                  '(((S (N (S x?))) (N x?)) ((N (N x?)) (I x?))
+                      ((S (I x?)) (S x?)) ((N (I x?) (N x?))))
+                  '(S (S (S (N (S (N (N (S (S (N x?))))))))))))
         (target '(I x?)))
     (if (equal? result target)
         #t
@@ -101,12 +110,16 @@
   (transform-exhaustive-iter a (list a)))
 
 ;;;; (pattern expression) -> expression
-;; This function applies the given pattern, if applicable, to the given expression, by matching
-;; either the whole expression, or each branch of the expression tree. If one part of the tree
-;; cannot be changed, but another can be changed, the unchangeable part is left as it was.
+;; This function applies the given pattern, if applicable, to the given
+;; expression, by matching either the whole expression, or each branch of the
+;; expression tree. If one part of the tree cannot be changed, but another can
+;; be changed, the unchangeable part is left as it was.
 ;;; Example-usage (transform-part-test)
-;;  ;; > (transform-part '((leaf?) (branch (leaf?) (leaf?)))
-;;  ;;                   '(branch (branch (leaf) (branch (leaf) (leaf))) (branch (branch (leaf))))
+;;  ;; > (transform-part
+;;  ;;      '((leaf?) (branch (leaf?) (leaf?)))
+;;  ;;      '(branch (branch (leaf)
+;;  ;;                       (branch (leaf) (leaf)))
+;;  ;;               (branch (branch (leaf))))
 ;;  ;; '(branch
 ;;  ;;   (branch
 ;;  ;;    (branch (leaf) (leaf))
@@ -117,8 +130,11 @@
 ;;  ;;   ((branch (branch (leaf))))
 ;;  ;;   ((branch (branch (leaf)))))
 (define (transform-part-test)
-  (let ((result (transform-part '((leaf?) (branch (leaf?) (leaf?)))
-                                '(branch (branch (leaf) (branch (leaf) (leaf))) (branch (branch (leaf))))))
+  (let ((result (transform-part
+                  '((leaf?) (branch (leaf?) (leaf?)))
+                  '(branch (branch (leaf)
+                                   (branch (leaf) (leaf)))
+                           (branch (branch (leaf))))))
         (target '(branch
                   (branch
                    (branch (leaf) (leaf))
@@ -136,16 +152,18 @@
         ((can-bind? (car pattern) a)
          (transform-total pattern a))
         ((can-bind? (car pattern) (car a))
-         (cons (transform-total pattern (car a)) (transform-part pattern (cdr a))))
+         (cons (transform-total pattern (car a))
+               (transform-part pattern (cdr a))))
         ((list? (car a))
          (cons (transform-part pattern (car a))
                (transform-part pattern (cdr a))))
         (else (cons (car a) (transform-part pattern (cdr a))))))
 
 ;;;; (pattern expression) -> expression
-;; Given a pattern and an expression, this function attempts to match the pattern to the entire expression
-;; at once. if the pattern is null, or the expression is null, then null is returned. If the pattern does
-;; not match the expression, then the original expression is returned unaltered.
+;; Given a pattern and an expression, this function attempts to match the
+;; pattern to the entire expression at once. if the pattern is null, or the
+;; expression is null, then null is returned. If the pattern does not match the
+;; expression, then the original expression is returned unaltered.
 ;;; Example-usage (transform-total-test)
 ;;  ;; > (transform-total '((square x?) (* x? x?))
 ;;  ;;                    '(square (sqrt (log 25))))
@@ -165,8 +183,9 @@
         (else a)))
 
 ;;;; (symbol) -> boolean
-;; Given a symbol, this function returns true if the symbol is formatted properly to be used as a variable
-;; in binding for pattern rewriting, and false otherwise.
+;; Given a symbol, this function returns true if the symbol is formatted
+;; properly to be used as a variable in binding for pattern rewriting, and false
+;; otherwise.
 ;;; Example-usage (bind-var?-test)
 ;;  ;; > (map bind-var? '(x? x 5))
 ;;  ;; '(#t #f #f)
@@ -183,15 +202,19 @@
        (var? x)))
 
 ;;;; (symbol scalar) -> boolean
-;; Given a symbol and a scalar, this function returns whether the symbol is a valid variable for matching
-;; constants, and whether the scalar given is a valid constant. A symbol is particularly used for matching
-;; constants if instead of having the suffix '?' it has the suffix '?c', and a scalar is a valid constant
-;; if it is neither a symbol (which could be a variable) nor a list (which would be better called a vector).
+;; Given a symbol and a scalar, this function returns whether the symbol is a
+;; valid variable for matching constants, and whether the scalar given is a
+;; valid constant. A symbol is particularly used for matching constants if
+;; instead of having the suffix '?' it has the suffix '?c', and a scalar is a
+;; valid constant if it is neither a symbol (which could be a variable) nor a
+;; list (which would be better called a vector).
 ;;; Example-usage (bind-const?-test)
-;;  ;; > (map (lambda (args) (apply bind-const? args)) '((x?c 5) (x?c (a b c)) (x? 5)))
+;;  ;; > (map (lambda (args) (apply bind-const? args))
+;;  ;;        '((x?c 5) (x?c (a b c)) (x? 5)))
 ;;  ;; '(#t #f #f)
 (define (bind-const?-test)
-  (let ((result (map (lambda (args) (apply bind-const? args)) '((x?c 5) (x?c (a b c)) (x? 5))))
+  (let ((result (map (lambda (args) (apply bind-const? args))
+                     '((x?c 5) (x?c (a b c)) (x? 5))))
         (target '(#t #f #f)))
     (if (equal? result target)
         #t
@@ -209,9 +232,10 @@
        (constant-var? x)))
 
 ;;;; (expr) -> list
-;; Given an expression, this function returns a list of all the variables in the expression tree, whether
-;; they be values on leaves or branches. The expression tree is assumed to be structured such that each
-;; node has at most two child branches.
+;; Given an expression, this function returns a list of all the variables in the
+;; expression tree, whether they be values on leaves or branches. The expression
+;; tree is assumed to be structured such that each node has at most two child
+;; branches.
 ;;; Example-usage (expr-vars-test)
 ;;  ;; > (expr-vars '(f? (g? x? y?) (h? a? (i? b?))))
 ;;  ;; '(f? g? x? y? h? a? i? b?)
@@ -233,17 +257,22 @@
               '()))))
 
 ;;;; (pattern expr) -> boolean
-;; This function returns true if the given pattern can bind to the given expression, and false otherwise.
-;; Note that a bind must be a complete match, not only a match at some sub-level of the expression tree.
+;; This function returns true if the given pattern can bind to the given
+;; expression, and false otherwise. Note that a bind must be a complete match,
+;; not only a match at some sub-level of the expression tree.
 ;;; Example-usage (can-bind?-test)
 ;;  ;; > (can-bind? '(subject? adverb? reporting-verb?
-;;  ;;                         (subject-accusative? subordinate-clause? infinitive?))
-;;  ;;              '((A hoplite) confidently said (Thermopylae (by Greek troops) (would be protected))))
+;;  ;;                  (subject-accusative? subordinate-clause? infinitive?))
+;;  ;;              '((A hoplite) confidently said
+;;  ;;                (Thermopylae (by Greek troops) (would be protected))))
 ;;  ;; #t
 (define (can-bind?-test)
-  (let ((result (can-bind? '(subject? adverb? reporting-verb?
-                                      (subject-accusative? subordinate-clause? infinitive?))
-                           '((A hoplite) confidently said (Thermopylae (by Greek troops) (would be protected)))))
+  (let ((result
+          (can-bind?
+            '(subject? adverb? reporting-verb?
+                (subject-accusative? subordinate-clause? infinitive?))
+            '((A hoplite) confidently said (Thermopylae (by Greek troops)
+              (would be protected)))))
         (target #t))
     (if (equal? result target)
         #t
@@ -262,12 +291,14 @@
        (not (null? (bindings pattern a)))))
 
 ;;;; (pattern expression) -> list
-;; Given a pattern and an expression, if the pattern matches the expression, this function returns
-;; the bindings mapping the variables in the pattern to their associated expressions within the
-;; expression tree.
+;; Given a pattern and an expression, if the pattern matches the expression,
+;; this function returns the bindings mapping the variables in the pattern to
+;; their associated expressions within the expression tree.
 ;;; Example-usage (bindings-test)
-;;  ;; > (bindings '(subject? adverb? reporting-verb? (subject-accusative? subordinate-clause? infinitive?))
-;;  ;;             '((A hoplite) confidently said (Thermopylae (by Greek troops) (would be protected))))
+;;  ;; > (bindings '(subject? adverb? reporting-verb?
+;;  ;;                (subject-accusative? subordinate-clause? infinitive?))
+;;  ;;             '((A hoplite) confidently said (Thermopylae (by Greek troops)
+;;  ;;               (would be protected))))
 ;;  ;; '((subject? (A hoplite))
 ;;  ;;   (adverb? confidently)
 ;;  ;;   (reporting-verb? said)
@@ -275,8 +306,12 @@
 ;;  ;;   (subordinate-clause? (by Greek troops))
 ;;  ;;   (infinitive? (would be protected)))
 (define (bindings-test)
-  (let ((result (bindings '(subject? adverb? reporting-verb? (subject-accusative? subordinate-clause? infinitive?))
-                          '((A hoplite) confidently said (Thermopylae (by Greek troops) (would be protected)))))
+  (let ((result
+          (bindings
+              '(subject? adverb? reporting-verb?
+                  (subject-accusative? subordinate-clause? infinitive?))
+              '((A hoplite) confidently said (Thermopylae (by Greek troops)
+                  (would be protected)))))
         (target '((subject? (A hoplite))
                   (adverb? confidently)
                   (reporting-verb? said)
@@ -291,7 +326,9 @@
     (cond ((null? a) '())
           ((or (bind-var? (car pattern))
                (bind-const? (car pattern) (car a)))
-           (acons (car pattern) (list (car a)) (bindings-aux (cdr pattern) (cdr a))))
+           (acons (car pattern)
+                  (list (car a))
+                  (bindings-aux (cdr pattern) (cdr a))))
           ((list? (car pattern))
            (append (bindings-aux (car pattern) (car a))
                    (bindings-aux (cdr pattern) (cdr a))))
